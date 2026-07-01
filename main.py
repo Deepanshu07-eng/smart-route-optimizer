70951378c02412df46839df9a5f32529fd981a3e
Deepanshu <d2004gautam@gmail.com>
2026-07-02 04:28:46 +0530

    Bus-route optimization project

---
+++ main.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Smart Bus Route System",
+    page_icon="🚌",
+    layout="wide"
+)
+
+st.title("🚌 Smart Bus Route System")
+st.write("Welcome Admin")
